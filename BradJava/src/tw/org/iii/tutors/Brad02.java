fbc8b574a264f24d8b6c60d15152d40269e9ca64
USER <USER@L39.r1804.tciii.org.tw>
2020-09-03 15:42:19 +0800

    202009031542

--- BradJava/src/tw/org/iii/tutors/Brad02.java
+++ BradJava/src/tw/org/iii/tutors/Brad02.java
@@ -2,21 +2,21 @@ package tw.org.iii.tutors;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Brad02 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("X = ");
-		int x = scanner.nextInt();
-		System.out.print("Y = ");
-		int y = scanner.nextInt();
-		int r1 = x + y;
-		int r2 = x - y;
-		int r3 = x * y;
-		int r4 = x / y;
-		int r5 = x % y;
-		System.out.println(String.format("%d + %d = %d",x,y,r1 ));
-		System.out.println(String.format("%d - %d = %d",x,y,r2 ));
-		System.out.println(String.format("%d x %d = %d",x,y,r3 ));
-		System.out.println(String.format("%d / %d = %d ... %d",x,y,r4, r5 ));
+		String strX = JOptionPane.showInputDialog("x = ?");
+		String strY = JOptionPane.showInputDialog("Y = ?");
+		
+		String r1 = strX + strY;
+//		int r2 = x - y;
+//		int r3 = x * y;
+//		int r4 = x / y;
+//		int r5 = x % y;
+		System.out.println(String.format("%s + %s = %s",strX,strY,r1 ));
+//		System.out.println(String.format("%d - %d = %d",x,y,r2 ));
+//		System.out.println(String.format("%d x %d = %d",x,y,r3 ));
+//		System.out.println(String.format("%d / %d = %d ... %d",x,y,r4, r5 ));
 	}
 }
