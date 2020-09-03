78b3121c3b5b949c65960f990b52f648f3169489
USER <USER@L39.r1804.tciii.org.tw>
2020-09-03 15:31:10 +0800

    202009031531

--- BradJava/src/tw/org/iii/tutors/Brad02.java
+++ BradJava/src/tw/org/iii/tutors/Brad02.java
@@ -9,7 +9,14 @@ public class Brad02 {
 		int x = scanner.nextInt();
 		System.out.print("Y = ");
 		int y = scanner.nextInt();
-		int r = x + y;
-		System.out.println(String.format("%d + %d = %d",x,y,r ));
+		int r1 = x + y;
+		int r2 = x - y;
+		int r3 = x * y;
+		int r4 = x / y;
+		int r5 = x % y;
+		System.out.println(String.format("%d + %d = %d",x,y,r1 ));
+		System.out.println(String.format("%d - %d = %d",x,y,r2 ));
+		System.out.println(String.format("%d x %d = %d",x,y,r3 ));
+		System.out.println(String.format("%d / %d = %d ... %d",x,y,r4, r5 ));
 	}
 }
