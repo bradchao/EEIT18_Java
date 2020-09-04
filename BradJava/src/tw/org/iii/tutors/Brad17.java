75605e2105063f9779bbf0863c452f2c5b54fda1
USER <USER@L39.r1804.tciii.org.tw>
2020-09-04 16:32:51 +0800

    202009041632

---
+++ BradJava/src/tw/org/iii/tutors/Brad17.java
@@ -0,0 +1,13 @@
+package tw.org.iii.tutors;
+
+public class Brad17 {
+
+	public static void main(String[] args) {
+		int a = -10, b = -3;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.printf("a = %d; b = %d", a, b);
+	}
+
+}
