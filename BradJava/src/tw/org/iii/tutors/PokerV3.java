6349fccb0381808f8e8d52d285b964b525dafba4
USER <USER@L39.r1803.tciii.org.tw>
2020-09-07 09:55:29 +0800

    202009070955

--- BradJava/src/tw/org/iii/tutors/PokerV3.java
+++ BradJava/src/tw/org/iii/tutors/PokerV3.java
@@ -15,21 +15,29 @@ public class PokerV3 {
 		}
 		
 		// for-each
-		for (int card: poker) {
-			System.out.println(card);
-		}
-		System.out.println("-------");
+//		for (int card: poker) {
+//			System.out.println(card);
+//		}
+//		System.out.println("-------");
 		//System.out.println(System.currentTimeMillis() - start);
 		
+		// 發牌
 		int[][] players = new int[4][13];
 		for (int i=0; i<poker.length; i++) {
 			players[i%4][i/4] = poker[i];
 		}
 
-		for (int card: players[1]) {
-			System.out.println(card);
+		// 攤牌
+		String[] suits = {"黑桃","紅心","方塊","梅花"};
+		String[] values = {"A ","2 ","3 ","4 ","5 ","6 ","7 ",
+						"8 ","9 ","10","J ","Q ","K "};
+		for (int[] player : players) {
+			for (int card: player) {
+				System.out.print(suits[card/13] + values[card%13] + " ");
+			}
+			System.out.println();
 		}
-		
+
 		
 		
 		
