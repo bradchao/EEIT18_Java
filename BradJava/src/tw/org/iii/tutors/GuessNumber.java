1a0b1f8e90bbc7f9ef698874eeef22f4efe0d9cb
USER <USER@L39.r1803.tciii.org.tw>
2020-09-08 13:55:57 +0800

    20209081355

--- BradJava/src/tw/org/iii/tutors/GuessNumber.java
+++ BradJava/src/tw/org/iii/tutors/GuessNumber.java
@@ -1,10 +1,35 @@
 package tw.org.iii.tutors;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class GuessNumber extends JFrame {
+	private JButton guess;
+	private JTextField input;
+	private JTextArea log;
+	
 	public GuessNumber() {
 		super("GuessNumber Game");
+		
+		setLayout(new BorderLayout());
+		
+		guess = new JButton("Guess");
+		input = new JTextField();
+		log = new JTextArea();
+		
+		add(log, BorderLayout.CENTER);
+		
+		JPanel topLine = new JPanel(new BorderLayout());
+		topLine.add(guess, BorderLayout.EAST);
+		topLine.add(input, BorderLayout.CENTER);
+		
+		add(topLine, BorderLayout.NORTH);
 		
 		setSize(640, 480);
 		setVisible(true);
